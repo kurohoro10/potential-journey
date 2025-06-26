e80ed59c09c2965d2b933877285252c17cf7e531
eduardobautista00 <eduardoculladobautista1113@gmail.com>
2025-06-26 16:24:38 +0800

    Git initialized in new PC.

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
 require_once 'core/init.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>test</h1>
+</body>
+</html>
