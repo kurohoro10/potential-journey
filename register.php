5f758ae73951e8a6bfcafefefa5bc002f3db26e8
kurohoro10 <kurohoro10@gmail.com>
2025-07-01 16:18:02 +0800

    -Added registration form to register.php

---
+++ register.php
@@ -0,0 +1,23 @@
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" value="" autocomplete="off" />
+    </div>
+
+    <div class="field">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" />
+    </div>
+
+    <div class="field">
+        <label for="password_again">Re-type password</label>
+        <input type="password" name="password_again" id="password_again" />
+    </div>
+
+    <div class="field">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" />
+    </div>
+
+    <input type="submit" value="Register" />
+</form>
