950a75ce276122c0a2c4535e9e63beed804bdc51
kurohoro10 <kurohoro10@gmail.com>
2025-07-01 13:52:53 +0800

    -Added update method.

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 require_once 'core/init.php';
 
-$user = DB::getInstance()->insert( 'users', array(
-    'username' => 'Dale',
-    'password' => 'password',
-    'salt'     => 'salt',
-    'name'     => 'Dale Garett',
+$user = DB::getInstance()->update( 'users', 3, array(
+    'username' => 'asdasdsadsa',
+    'password' => 'adsasdsadsa',
+    'salt'     => 'asdsadsad',
+    'name'     => 'asdasdsa',
     'joined'   => date('Y-m-d H:i:s'),
     'grouped'  => 1
 ));
