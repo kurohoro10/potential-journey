5e1c66fd700a4b589e018e22293a38c4e195d0fd
kurohoro10 <kurohoro10@gmail.com>
2025-07-02 11:10:02 +0800

    -Added comment block for exists method. -Added get method to retrieve specific item.

--- classes/Input.php
+++ classes/Input.php
@@ -1,5 +1,12 @@
 <?php
 class Input {
+    /**
+     * Get a value from the $_POST or $_GET superglobals.
+     *
+     * @param string $key The key to retrieve.
+     * @param string $type The type of input ('post' or 'get').
+     * @return mixed The value if found, null otherwise.
+     */
     public static function exists( $type = 'post' ) {
         switch ( $type ) {
             case 'post':
@@ -14,5 +21,18 @@ class Input {
         }
     }
 
-    
+    /**
+     * Get the value of a specific item from POST or GET data.
+     *
+     * @param string $item The name of the item to retrieve.
+     * @return mixed The value of the item if it exists, false otherwise.
+     */
+    public static function get( $item ) {
+        if ( isset( $_POST[$item] ) ) {
+            return $_POST[$item];
+        } else if ( iset( $_GET[$item] ) ) {
+            return $_GET[$item];
+        }
+        return false;
+    }
 }
