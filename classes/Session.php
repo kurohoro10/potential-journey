4266611db962049110e2e16c0edfa4ec3f12a132
kurohoro10 <kurohoro10@gmail.com>
2025-07-05 21:11:04 +0800

    Added comment block to Session class.

--- classes/Session.php
+++ classes/Session.php
@@ -1,20 +1,56 @@
 <?php
+/**
+ * Class Session
+ *
+ * A simple static class for handling PHP session data.
+ * It provides convenient methods to check, set, get, and delete session variables.
+ *
+ * Methods:
+ * - exists($name): Check if a session variable exists.
+ * - put($name, $value): Set a session variable.
+ * - get($name): Retrieve a session variable.
+ * - delete($name): Remove a session variable if it exists.
+ */
 class Session {
-    public static function exists( $name ) {
-        return ( isset( $_SESSION[$name] ) ) ? true : false;
+    /**
+     * Check if a session variable exists.
+     *
+     * @param string $name The name of the session variable.
+     * @return bool True if the variable exists, false otherwise.
+     */
+    public static function exists($name) {
+        return isset($_SESSION[$name]);
     }
 
-    public static function put( $name, $value ) {
+    /**
+     * Set a session variable.
+     *
+     * @param string $name The name of the session variable.
+     * @param mixed $value The value to assign.
+     * @return mixed The value that was set.
+     */
+    public static function put($name, $value) {
         return $_SESSION[$name] = $value;
     }
 
-    public static function get( $name ) {
-        return $_SESSION[ $name ];
+    /**
+     * Get the value of a session variable.
+     *
+     * @param string $name The name of the session variable.
+     * @return mixed|null The value of the variable, or null if not set.
+     */
+    public static function get($name) {
+        return isset($_SESSION[$name]) ? $_SESSION[$name] : null;
     }
 
-    public static function delete( $name ) {
-        if ( self::exists( $name ) ) {
-            unset( $_SESSION[$name] );
+    /**
+     * Delete a session variable if it exists.
+     *
+     * @param string $name The name of the session variable.
+     */
+    public static function delete($name) {
+        if (self::exists($name)) {
+            unset($_SESSION[$name]);
         }
     }
 }
