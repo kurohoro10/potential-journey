cf866df97392a35ab02b761e34832704e68385c4
kurohoro10 <kurohoro10@gmail.com>
2025-07-18 18:47:32 +0800

    Added assets.

--- index.php
+++ index.php
@@ -10,29 +10,44 @@ $user = new User();
 
 if ($user->isLoggedIn()) {
     ?>
-        <p>Hello <a href="profile.php?user=<?php echo htmlspecialchars($user->data()->username); ?>"><?php echo htmlspecialchars($user->data()->username); ?></a>!</p>
 
-        <ul>
-            <li><a href="logout.php">Log out</a></li>
-            <li><a href="update.php">Update details</a></li>
-            <li><a href="changepassword.php">Change password</a></li>
-        </ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo require_once 'assets/css/style.css'; ?>">
+    <script defer src="/assets/js/script.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <p>Hello <a href="profile.php?user=<?php echo htmlspecialchars($user->data()->username); ?>"><?php echo htmlspecialchars($user->data()->username); ?></a>!</p>
+
+    <ul>
+        <li><a href="logout.php">Log out</a></li>
+        <li><a href="update.php">Update details</a></li>
+        <li><a href="changepassword.php">Change password</a></li>
+    </ul>
+
     <?php
 
-    if ($user->hasPermission('admin')) {
-        echo '<p>You are an admin!</p>';
-    }
+        if ($user->hasPermission('admin')) {
+            echo '<p>You are an admin!</p>';
+        }
 
-    if ($user->hasPermission('moderator')) {
-        echo '<p>You are an moderator!</p>';
-    }
+        if ($user->hasPermission('moderator')) {
+            echo '<p>You are an moderator!</p>';
+        }
 
-} else {
+        } else {
+            ?>
+                <p>You need to 
+                    <a href="login.php">log in</a> 
+                    or 
+                    <a href="register.php">register</a>
+                </p>
+            <?php
+        }
     ?>
-        <p>You need to 
-            <a href="login.php">log in</a> 
-            or 
-            <a href="register.php">register</a>
-        </p>
-    <?php
-}
+</body>
+</html>
